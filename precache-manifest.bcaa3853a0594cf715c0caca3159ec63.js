b23cf106af7458787a9a298ef66b970218949b7a
Joseph Ramli <joseph.ramli@binus.ac.id>
2020-05-13 18:45:15 +0700

    Updates

---
+++ precache-manifest.bcaa3853a0594cf715c0caca3159ec63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a282b8dc639ab0fe42c84bb3d3cceef",
+    "url": "/robo-monster/index.html"
+  },
+  {
+    "revision": "c8e8841209f8af63df49",
+    "url": "/robo-monster/static/css/main.408c1099.chunk.css"
+  },
+  {
+    "revision": "adf942f5cfef5dae22ad",
+    "url": "/robo-monster/static/js/2.dcb032b6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robo-monster/static/js/2.dcb032b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8e8841209f8af63df49",
+    "url": "/robo-monster/static/js/main.7e64cc50.chunk.js"
+  },
+  {
+    "revision": "ae011453fb124b19a253",
+    "url": "/robo-monster/static/js/runtime-main.5e98acad.js"
+  }
+]);
